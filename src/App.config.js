3f8bcaab6f42d903d0cdf8f07b62cfe8a2a5f9a4
Fahrur Rifai <mfahrurrifai@gmail.com>
2024-07-20 17:13:22 +0700

    feat: Update environment variables and dependencies

--- src/App.config.js
+++ src/App.config.js
@@ -1,5 +1,5 @@
 const apiURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
-const publicURL = process.env.PUBLIC_URL || 'http://localhost:3001';
+const publicURL = process.env.REACT_APP_PUBLIC_URL || 'http://localhost:3001';
 
 export {
   apiURL,
